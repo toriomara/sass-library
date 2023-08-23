1f3a152b87fd8a468aeec5292ee37f59b6aebf14
toriomara <budkevich.vladimir@gmail.com>
2023-08-23 14:25:39 +0300

    feat: finish course

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-  return src('index.scss')
+  return src('sass/**/*.scss')
     .pipe(sass())
+    .pipe(purgecss({content: ['*.html']}))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles)
+  watch(['sass/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask);
